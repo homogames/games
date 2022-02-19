fc7efbc788518ecccf892153d4e27aeba716bd59
Homo <100041027+homogames@users.noreply.github.com>
2022-02-19 21:50:04 +0400

    initial

---
+++ manokplay/1/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1551706167,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/grass_side.png",
+		"images/log_oak.png",
+		"images/leaves.png",
+		"images/wood.png",
+		"images/ladder.png",
+		"images/tnt.png",
+		"images/door-sheet0.png",
+		"images/wood_back.png",
+		"images/furnace.png",
+		"images/win_furnace.png",
+		"images/enemy-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ vova/beta/1/offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1547222538,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/vova-sheet0.png",
+		"images/pol.png",
+		"images/stena.png",
+		"images/shkaf-sheet0.png",
+		"images/parta-sheet0.png",
+		"images/stul-sheet0.png",
+		"images/ilyash-sheet0.png",
+		"images/matvey-sheet0.png",
+		"images/glasses1-sheet0.png",
+		"images/tiledbackground.png",
+		"images/auto-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/jblcharge2une-sheet0.png",
+		"images/up-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/tiledbackground3.png",
+		"images/sprite3-sheet0.png",
+		"media/untitled.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
